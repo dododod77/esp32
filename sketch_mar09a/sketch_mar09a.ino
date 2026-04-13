600313337e956bb835e22e3e6a1a1f46586678ba
dododod77 <narendraandrian20@gmail.com>
2026-04-13 13:54:59 +0700

    Add files via upload

---
+++ sketch_mar09a/sketch_mar09a.ino
@@ -0,0 +1,550 @@
+#define a 26
+#define b 27
+#define c 18
+#define d 17
+#define e 16
+#define f 25
+#define g 33
+
+int GND1 = 15;
+int GND2 = 2;
+
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+
+  pinMode(GND1,OUTPUT);
+  pinMode(GND2,OUTPUT);
+}
+
+void loop() {
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  bLet();
+  delay(10);
+
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  buLet();
+  delay(10);
+
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  udLet();
+  delay(10);
+
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  diLet();
+  delay(10);
+
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  iLet();
+  delay(10);
+
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  offline();
+  delay(10);
+
+}
+
+void offline(){
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+}
+
+void a0(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+}
+
+void a1(){
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+}
+
+void a2(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+}
+
+void a3(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+}
+
+void a4(){
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void a5(){
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void a6(){
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void a7(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+}
+
+void a8(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void a9(){
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void ak3(){
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void ded(){
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+}
+
+void  B(){
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+}
+
+void U(){
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+}
+
+void D(){
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+}
+
+void a12() {
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  a1();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  a2();
+  delay(10);
+}
+
+void a34() {
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  a3();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  a4();
+  delay(10);
+}
+
+void OFFLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  offline();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  offline();
+  delay(10);
+}
+
+void bLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  offline();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  B();
+  delay(10);
+}
+
+void buLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  B();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  U();
+  delay(10);
+}
+
+void udLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  U();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  D();
+  delay(10);
+}
+
+void diLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  D();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  a1();
+  delay(10);
+}
+
+void iLet(){
+  digitalWrite(GND1,0);
+  digitalWrite(GND2,1);
+
+  a1();
+  delay(10);
+
+  digitalWrite(GND1,1);
+  digitalWrite(GND2,0);
+
+  offline();
+  delay(10);
+}
